70ecba026e8aed6723867b3c811fd7182e7d5281
Amit Agrahari <amit.agrahari@nagarro.com>
2020-06-29 02:30:23 +0530

    React Hook ,useRef

---
+++ react-useref-hooks/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import FocusInput from './components/FocusInput';
+import HookTimer from './components/HookTimer';
+
+function App() {
+  return (
+    <div className="App">
+      {/** useRef */}
+      {/* <FocusInput/> */}
+
+      {/** useRef */}
+      <HookTimer/>
+    </div>
+  );
+}
+
+export default App;
